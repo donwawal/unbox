dc296240fe5316fa496e6ada14473cb4362eeddb
Jules <jules.walter@gmail.com>
2015-06-08 20:33:47 -0700

    Initial Commit

--- unbox/ViewController.swift
+++ unbox/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  unbox
 //
-//  Created by Jules Walter on 6/8/15.
+//  Created by Christine Chang, Court Kizer, and Jules Walter,  on 6/8/15.
 //  Copyright (c) 2015 Jules Walter. All rights reserved.
 //
 
